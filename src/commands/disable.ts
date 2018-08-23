a7233e342efceeaf38505a06baaac05ecc1f0e41
Brandon Kalinowski <brandon@brandonkalinowski.com>
2018-08-23 16:44:17 -0700

    prettier

--- src/commands/disable.ts
+++ src/commands/disable.ts
@@ -1,28 +1,27 @@
-import { Command, flags } from '@oclif/command';
 import { removeDomain, hasCertificateFor } from 'devcert';
-import { writeFileSync as write } from 'fs';
+import { Command, flags } from '@oclif/command'
+import { writeFileSync as write } from 'fs'
 
 export default class Generate extends Command {
+  static description = 'delete the cached certificate for a domain'
 
-  static description = 'delete the cached certificate for a domain';
+  static flags = {}
 
-  static flags = {};
-
-  static args = [ { name: 'domain' } ];
+  static args = [{ name: 'domain' }]
 
   async run() {
-    const { args } = this.parse(Generate);
+    const { args } = this.parse(Generate)
 
     if (!args.domain) {
-      this.error('You must supply a domain name that you want to disable.');
+      this.error('You must supply a domain name that you want to disable.')
     }
 
     if (!hasCertificateFor(args.domain)) {
-      this.error(`${ args.domain } doesn't appear to be configured yet.`);
+      this.error(`${args.domain} doesn't appear to be configured yet.`)
     }
 
-    removeDomain(args.domain);
+    removeDomain(args.domain)
 
-    this.log(`${ args.domain } deleted`);
+    this.log(`${args.domain} deleted`)
   }
 }
